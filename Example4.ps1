7486763f286e96c7b239ed086672a03cfb2be6ee
DrHalfBaked <94751370+DrHalfBaked@users.noreply.github.com>
2021-11-25 19:39:11 +0200

    Add files via upload

---
+++ Example4.ps1
@@ -0,0 +1,58 @@
+###########
+#  Learn how to build Material Design based PowerShell apps
+#  --------------------
+#  Example4 Data Tables
+#  --------------------
+#  Avi Coren (c)
+#  Blog     - https://avicoren.wixsite.com/powershell
+#  Github   - https://github.com/DrHalfBaked/PowerShell
+#  LinkedIn - https://www.linkedin.com/in/avi-coren-6647b2105/
+#
+[Void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[Void][System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\Assembly\MaterialDesignThemes.Wpf.dll")
+[Void][System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\Assembly\MaterialDesignColors.dll")
+
+try {
+    [xml]$Xaml = (Get-content "$PSScriptRoot\Example4.xaml")
+    $Reader = New-Object System.Xml.XmlNodeReader $Xaml
+    $Window = [Windows.Markup.XamlReader]::Load($Reader)
+} 
+catch {
+    Write-Error "Error building Xaml data.`n$_"
+    exit
+}
+
+$Xaml.SelectNodes("//*[@Name]") | ForEach-Object { Set-Variable -Name ($_.Name) -Value $Window.FindName($_.Name) -Scope Script }
+
+
+$Services_Datatable = [System.Data.DataTable]::New()
+[void]$Services_Datatable.Columns.AddRange(@('Name', 'Description', 'State', 'StartMode'))
+
+Function FilterServices {
+    $FilterString = [System.Text.StringBuilder]::New()
+    if ( $Services_TxtBox_FilterByName.Text) { 
+        $FilterString.Append(" Name LIKE '%$($Services_TxtBox_FilterByName.Text)%' AND ") 
+    }
+    if ($Services_ChkBox_FilterRunning.IsChecked) { 
+        $FilterString.Append("State LIKE 'Running'") 
+    }
+    else {
+        $FilterString.Append("State LIKE '%%'")
+    }
+    $Services_Datatable.DefaultView.RowFilter = $FilterString
+    $Services_DataGrid.ItemsSource = $Services_Datatable.DefaultView
+}
+
+$Services = Get-CimInstance -ClassName Win32_service | Select-Object name, description, state, startmode
+
+foreach ($Service in $Services) {
+    [void]$Services_Datatable.Rows.Add($Service.Name, $Service.Description, $Service.State, $Service.StartMode)
+}
+
+$Services_DataGrid.ItemsSource = $Services_Datatable.DefaultView
+
+$Services_TxtBox_FilterByName.Add_TextChanged({ FilterServices })
+$Services_ChkBox_FilterRunning.Add_Checked({ FilterServices })
+$Services_ChkBox_FilterRunning.Add_Unchecked({ FilterServices })
+
+$Window.ShowDialog() | out-null
