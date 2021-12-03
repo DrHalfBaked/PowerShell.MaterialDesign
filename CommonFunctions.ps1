296471504b2cc0e56639f00991e9b9407d933b83
DrHalfBaked <94751370+DrHalfBaked@users.noreply.github.com>
2021-12-03 16:10:35 +0200

    Add files via upload

---
+++ CommonFunctions.ps1
@@ -0,0 +1,116 @@
+###########
+#  Learn how to build Material Design based PowerShell apps
+# 
+#  Common Functions and Assemblies
+#
+#  Avi Coren (c)
+#  Blog     - https://avicoren.wixsite.com/powershell
+#  Github   - https://github.com/DrHalfBaked/PowerShell
+#  LinkedIn - https://www.linkedin.com/in/avi-coren-6647b2105/
+#
+#  Last file update:  Dec 3, 2021  14:41
+#
+[Void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[Void][System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\Assembly\MaterialDesignThemes.Wpf.dll")
+[Void][System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\Assembly\MaterialDesignColors.dll")
+
+function New-Window {
+    param (
+        [Parameter()]    
+        $XamlFile,
+        [Parameter()]
+        [Switch]$NoSnackbar
+    )
+        
+    try {
+        [xml]$Xaml = (Get-content $XamlFile)
+        $Reader = New-Object System.Xml.XmlNodeReader $Xaml
+        $Window = [Windows.Markup.XamlReader]::Load($Reader)
+    } 
+    catch {
+        Write-Error "Error building Xaml data.`n$_"
+        exit
+    }
+    
+    $Xaml.SelectNodes("//*[@Name]") | ForEach-Object { Set-Variable -Name ($_.Name) -Value $Window.FindName($_.Name) -Scope Script }
+
+    # Objects that have to be declared before the window launches (will run in the same dispatcher thread as the window)
+    if (!$NoSnackbar){
+        $Script:MessageQueue =   [MaterialDesignThemes.Wpf.SnackbarMessageQueue]::new()
+        $Script:MessageQueue.DiscardDuplicates = $true
+    }
+
+    return $Window
+}
+
+function New-Snackbar {
+    param (
+        $Snackbar,
+        $Text,
+        $ButtonCaption
+    )
+    try{
+        if ($ButtonCaption) {       
+            $MessageQueue.Enqueue($Text, $ButtonCaption, {$null}, $null, $false, $false, [TimeSpan]::FromHours( 9999 ))
+        }
+        else {
+            $MessageQueue.Enqueue($Text, $null, $null, $null, $false, $false, $null)
+        }
+        $Snackbar.MessageQueue = $MessageQueue
+    }
+    catch{
+        Write-Error "No MessageQueue was declared in the window. Make sure -NoSnackbar switch wasn't used in New-Window`n$_"
+    }
+}
+
+function Set-NavigationRailTab {
+    param (
+        $NavigationRail,
+        $TabName
+    )
+    $NavigationRail.SelectedIndex = [array]::IndexOf((($NavigationRail.Items | Select-Object -ExpandProperty name).toupper()), $TabName.ToUpper())
+}
+
+function Get-NavigationRailSelectedTabName {
+    param (
+        $NavigationRail
+    )
+    return $NavigationRail.Items | Where-Object {$_.IsSelected -eq "True"} | Select-Object -ExpandProperty name
+}
+
+function Get-SaveFilePath {
+    Param (
+        [string] $InitialDirectory,
+        [string] $Filter
+    )
+    try {
+        $SaveFileDialog = New-Object Microsoft.Win32.SaveFileDialog
+        $SaveFileDialog.initialDirectory = $initialDirectory
+        $SaveFileDialog.filter = $Filter
+        $SaveFileDialog.CreatePrompt = $False;
+        $SaveFileDialog.OverwritePrompt = $True;
+        $SaveFileDialog.ShowDialog() | Out-Null
+        return $SaveFileDialog.filename
+    } 
+    catch {
+        Throw "Save-File Error $_"
+    }
+} 
+
+function Get-OpenFilePath {
+    Param (
+        [string] $InitialDirectory,
+        [string] $Filter
+    )
+    try{
+        $OpenFileDialog = New-Object Microsoft.Win32.OpenFileDialog
+        $OpenFileDialog.initialDirectory = $initialDirectory
+        $OpenFileDialog.filter = $Filter
+        # Examples of other common filters: "Word Documents|*.doc|Excel Worksheets|*.xls|PowerPoint Presentations|*.ppt |Office Files|*.doc;*.xls;*.ppt |All Files|*.*"
+        $OpenFileDialog.ShowDialog() | Out-Null
+        return $OpenFileDialog.filename
+    }
+    catch {
+        Throw "Open-File Error $_"
+    }
+} 
---
+++ Example8.ps1
@@ -0,0 +1,57 @@
+###########
+#  Learn how to build Material Design based PowerShell apps
+#  --------------------
+#  Example8: Navigation Rails - Tab awareness + introducing common files
+#  --------------------
+#  Avi Coren (c)
+#  Blog     - https://avicoren.wixsite.com/powershell
+#  Github   - https://github.com/DrHalfBaked/PowerShell
+#  LinkedIn - https://www.linkedin.com/in/avi-coren-6647b2105/
+#
+Get-ChildItem -Path $PSScriptRoot -Filter Common*.PS1 | ForEach-Object {. ($_.FullName)}
+
+$Window = New-Window -XamlFile "$PSScriptRoot\Example8.xaml"
+
+[scriptblock]$SyncDrawerSizeWithWindow = {
+    $DrawerHost.Height = $MainWindow.Height  
+    $DrawerHost.Width = $MainWindow.Width
+}
+
+[scriptblock]$OnClosingDrawer = {
+    $DrawerHost.IsLeftDrawerOpen = $false
+    $TglBtn_OpenLeftDrawer.IsChecked = $false
+    $TglBtn_OpenLeftDrawer.Visibility="Visible"
+}
+
+$MainWindow.add_SizeChanged($SyncDrawerSizeWithWindow)
+
+$DrawerHost.add_DrawerOpened($SyncDrawerSizeWithWindow)
+$DrawerHost.add_DrawerClosing($OnClosingDrawer)
+
+$TglBtn_CloseLeftDrawer.add_Click($OnClosingDrawer)
+
+$TglBtn_OpenLeftDrawer.add_Click({ 
+    $DrawerHost.IsLeftDrawerOpen = $true
+    $TglBtn_CloseLeftDrawer.IsChecked = $true
+    $TglBtn_OpenLeftDrawer.Visibility="Hidden"
+})
+
+$LeftDrawer_ListItem1.add_Selected({
+    Set-NavigationRailTab -NavigationRail $NavRail -TabName "Settings"
+})
+
+$LeftDrawer_ListItem2.add_Selected({
+    $OpeneFilePath = Get-OpenFilePath -InitialDirectory $InitialDirectory  -Filter $FileFilter 
+    if ($OpeneFilePath) {
+        New-Snackbar -Snackbar $Snackbar1 -Text "You selected $OpeneFilePath"
+    }
+})
+
+$LeftDrawer_ListItem3.add_Selected({
+    $Window.Close()
+})
+
+$NavRail.add_SelectionChanged({ New-Snackbar -Snackbar $Snackbar1 -Text "You selected the $(Get-NavigationRailSelectedTabName -NavigationRail $NavRail) page" })
+$LeftDrawerListBox1.add_SelectionChanged({ $LeftDrawerListBox1.SelectedIndex = -1 })
+
+$Window.ShowDialog() | out-null
