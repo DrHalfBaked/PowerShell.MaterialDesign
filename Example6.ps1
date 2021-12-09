334127a1acc09d1343d326ba37f8f1b5ea719366
DrHalfBaked <94751370+DrHalfBaked@users.noreply.github.com>
2021-12-09 19:25:41 +0200

    Add files via upload

---
+++ Example6.ps1
@@ -0,0 +1,27 @@
+###########
+#  Learn how to build Material Design based PowerShell apps
+#  --------------------
+#  Example6: Tab Control (Navigation Rails)
+#  --------------------
+#  Avi Coren (c)
+#  Blog     - https://avicoren.wixsite.com/powershell
+#  Github   - https://github.com/DrHalfBaked/PowerShell.MaterialDesign
+#  LinkedIn - https://www.linkedin.com/in/avi-coren-6647b2105/
+#
+[Void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[Void][System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\Assembly\MaterialDesignThemes.Wpf.dll")
+[Void][System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\Assembly\MaterialDesignColors.dll")
+
+try {
+    [xml]$Xaml = (Get-content "$PSScriptRoot\Example6.xaml")
+    $Reader = New-Object System.Xml.XmlNodeReader $Xaml
+    $Window = [Windows.Markup.XamlReader]::Load($Reader)
+} 
+catch {
+    Write-Error "Error building Xaml data.`n$_"
+    exit
+}
+
+$Xaml.SelectNodes("//*[@Name]") | ForEach-Object { Set-Variable -Name ($_.Name) -Value $Window.FindName($_.Name) -Scope Script }
+
+$Window.ShowDialog() | out-null
