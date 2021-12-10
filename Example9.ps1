fd2d1106cbb158a444c3b0a924f7340791e59044
DrHalfBaked <94751370+DrHalfBaked@users.noreply.github.com>
2021-12-10 18:14:37 +0200

    Add files via upload

--- Example9.ps1
+++ Example9.ps1
@@ -15,25 +15,24 @@ $Window = New-Window -XamlFile "$PSScriptRoot\Example9.xaml"
 
 #####Set-Culture -CultureInfo en-IL #(Get-WinSystemLocale | Select-Object -ExpandProperty Name)
 
-Get-Variable -Include "Form_Textbox_*" -ValueOnly | ForEach-Object { Set-OutlinedProperty -TextboxObject $_ -Padding "2" -SetFloatingOffset "5,-18" -SetFloatingScale "0.8" -FontSize 16 }
-Get-Variable -Include "Form_TextboxShort_*","Form_Combobox_*","Form_TimePicker_*","Form_DatePicker_*"  -ValueOnly | ForEach-Object { Set-OutlinedProperty -TextboxObject $_ -Padding "8" -SetFloatingOffset "1,-18" -SetFloatingScale "0.8" -FontSize 16 }
+Get-Variable -Include "Car_Reg_Form_Textbox_*" -ValueOnly |
+    ForEach-Object { Set-OutlinedProperty -TextboxObject $_ -Padding "2" -SetFloatingOffset "5,-18" -SetFloatingScale "0.8" -FontSize 16 }
+Get-Variable -Include "Car_Reg_Form_TextboxShort_*","Car_Reg_Form_Combobox_*","Car_Reg_Form_TimePicker_*","Car_Reg_Form_DatePicker_*"  -ValueOnly |
+    ForEach-Object { Set-OutlinedProperty -TextboxObject $_ -Padding "8" -SetFloatingOffset "1,-18" -SetFloatingScale "0.8" -FontSize 16 }
 
 $CarList = Import-Csv -Path "$PSScriptRoot\Cars.csv"
-Add-ItemToUIControl -UIControl $Form_Combobox_Car_Make -ItemToAdd ($CarList.Make | Select-Object -Unique | Sort-Object )
-Add-ItemToUIControl -UIControl $Form_Combobox_Car_Color -ItemToAdd ([System.Windows.Media.Colors].Getproperties() | Select-Object -ExpandProperty name | Sort-Object )
-Add-ItemToUIControl -UIControl $Form_Combobox_Car_Year -ItemToAdd (1990..2021)
-$Form_Combobox_Car_Make.add_SelectionChanged({
-    Add-ItemToUIControl -UIControl $Form_Combobox_Car_Model -Clear
-    Add-ItemToUIControl -UIControl $Form_Combobox_Car_Model -ItemToAdd ($CarList | Where-Object {$_.Make -eq $Form_Combobox_Car_Make.SelectedItem }  | Select-Object -ExpandProperty Model | Select-Object -Unique | Sort-Object )
+Add-ItemToUIControl -UIControl $Car_Reg_Form_Combobox_Make -ItemToAdd ($CarList.Make | Select-Object -Unique | Sort-Object )
+Add-ItemToUIControl -UIControl $Car_Reg_Form_Combobox_Color -ItemToAdd ([System.Windows.Media.Colors].Getproperties() | Select-Object -ExpandProperty name | Sort-Object )
+Add-ItemToUIControl -UIControl $Car_Reg_Form_Combobox_Year -ItemToAdd (1990..2021)
+
+$Car_Reg_Form_Combobox_Make.add_SelectionChanged({
+    Add-ItemToUIControl -UIControl $Car_Reg_Form_Combobox_Model -Clear
+    Add-ItemToUIControl -UIControl $Car_Reg_Form_Combobox_Model -ItemToAdd ($CarList | Where-Object {$_.Make -eq $Car_Reg_Form_Combobox_Make.SelectedItem }  | Select-Object -ExpandProperty Model | Select-Object -Unique | Sort-Object )
 })
 
-
 $Cars_Datatable = [System.Data.DataTable]::New()
-[void]$Cars_Datatable.Columns.AddRange(@('Make', 'Model', 'Owner', 'Color'))
-$Cars_Datatable.primarykey = $Cars_Datatable.columns['Make', 'Model', 'Owner', 'Color']
-[void]$Cars_Datatable.Rows.Add('Toyota', 'Corolla', 'John Smith', 'Blue')
-[void]$Cars_Datatable.Rows.Add('Tesla', 'Model 3', 'Will Bond', 'White')
-
+[void]$Cars_Datatable.Columns.AddRange(@('Plate', 'Make', 'Model', 'Year', 'Owner', 'Color', 'Note', 'Rating', 'DateReg', 'TimeReg'))
+$Cars_Datatable.primarykey = $Cars_Datatable.columns['Plate']
 $Cars_DataGrid.ItemsSource = $Cars_Datatable.DefaultView
 
 [scriptblock]$OnClosingDrawer = {
@@ -51,28 +50,46 @@ $TglBtn_OpenLeftDrawer.add_Click({
     $TglBtn_OpenLeftDrawer.Visibility="Hidden"
 })
 
-$Cars_Popup_Add_Car.Add_Click( { $NavRail.SelectedIndex = [array]::IndexOf((($NavRail.Items | Select-Object -ExpandProperty name).toupper()),"CarRegistration".ToUpper()) })
+$Cars_Popup_Add_Car.Add_Click({ Set-NavigationRailTab -NavigationRail $NavRail -TabName "CarRegistration" })
 
 [scriptblock]$OnResetCarForm = {
-    Get-Variable -Include "Form_Combobox*Car*" -ValueOnly | ForEach-Object {$_.SelectedItem = $null}
-    Get-Variable -Include "Form_Textbox*Car*","Form_DatePicker*Car*","Form_TimePicker*Car*" -ValueOnly | ForEach-Object { $_.Text = $null }
-    $Form_RatingBar_Car_Rate.Value = 1
+    Get-Variable -Include "Car_Reg_Form_Combobox*" -ValueOnly | ForEach-Object {$_.SelectedItem = $null}
+    Get-Variable -Include "Car_Reg_Form_Textbox*","Car_Reg_Form_DatePicker*","Car_Reg_Form_TimePicker*" -ValueOnly | ForEach-Object { $_.Text = $null }
+    $Car_Reg_Form_RatingBar_Rate.Value = 1
 }
 
 $NavRail.add_SelectionChanged({
     if ($_.Source -like "System.Windows.Controls.TabControl*") {
-        Write-host " tab control event"
         switch (Get-NavigationRailSelectedTabName -NavigationRail $NavRail) {
 
             "CarRegistration"   {   Invoke-Command -Command $OnResetCarForm
-                                    $Form_Btn_Apply.IsEnabled = $false 
+                                    #$Car_Reg_Form_Btn_Apply.IsEnabled = $false 
                                 }
-            
         }
     }
 })
 
-$Form_Btn_Reset.add_Click($OnResetCarForm)
-$Form_Btn_Cancel.add_Click({ Set-NavigationRailTab -NavigationRail $NavRail -TabName "Cars" })
+$Car_Reg_Form_Btn_Reset.add_Click($OnResetCarForm)
+$Car_Reg_Form_Btn_Cancel.add_Click({ Set-NavigationRailTab -NavigationRail $NavRail -TabName "Cars" })
+$Car_Reg_Form_Btn_Apply.add_Click({
+    $RowContent = @(
+        $Car_Reg_Form_TextboxShort_Plate.Text
+        $Car_Reg_Form_Combobox_Make.SelectedItem
+        $Car_Reg_Form_Combobox_Model.SelectedItem
+        $Car_Reg_Form_Combobox_Year.SelectedItem
+        $Car_Reg_Form_Textbox_Owner.Text
+        $Car_Reg_Form_Combobox_Color.SelectedItem
+        $Car_Reg_Form_TextboxShort_Note.Text
+        $Car_Reg_Form_RatingBar_Rate.Value
+        $Car_Reg_Form_DatePicker_Registered.Text
+        $Car_Reg_Form_TimePicker_Registered.Text
+    )
+    [void]$Cars_Datatable.Rows.Add($RowContent)
+    Set-NavigationRailTab -NavigationRail $NavRail -TabName "Cars"
+})
+
+
+
+
 
 $Window.ShowDialog() | out-null
