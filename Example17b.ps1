52d56140f7ed79e2ba1072d8d314e6097df1f415
DrHalfBaked <94751370+DrHalfBaked@users.noreply.github.com>
2022-01-17 19:33:36 +0200

    Add files via upload

--- Example17b.ps1
+++ Example17b.ps1
@@ -13,40 +13,58 @@ Get-ChildItem -Path $PSScriptRoot -Filter Common*.PS1 | ForEach-Object {. ($_.Fu
 $Window = New-Window -XamlFile "$PSScriptRoot\Example17b.xaml"
 
 $Services_Datatable = [System.Data.DataTable]::New()
-[void]$Services_Datatable.Columns.AddRange(@('CheckboxChecked','Name', 'Description', 'State', 'StartMode'))
+[void]$Services_Datatable.Columns.Add('CheckboxSelect',[bool])
+[void]$Services_Datatable.Columns.AddRange(@('Name', 'Description', 'State', 'StartMode'))
+######$Services_Datatable.Columns.PrimaryKey = 'Name'
 $Services_DataGrid.ItemsSource = $Services_Datatable.DefaultView
 
 $Services = Get-CimInstance -ClassName Win32_service | Select-Object name, description, state, startmode
 
 foreach ($Service in $Services) {
-    [void]$Services_Datatable.Rows.Add($true,$Service.Name, $Service.Description, $Service.State, $Service.StartMode)
+    [void]$Services_Datatable.Rows.Add($false,$Service.Name, $Service.Description, $Service.State, $Service.StartMode)
 }
 
-$Services_HeaderChkBox.add_Checked({ Write-Host "kuku" })
-
-<# 
-$HiddenInsertButton.add_Click({
-   
-        Write-Host "Clicked row $($Services_DataGrid.SelectedItem.Row.Name)"
+$Services_HeaderChkBox.add_Checked({ 
+    $Services_Datatable | ForEach-Object {$_.CheckboxSelect = $true}
+    #$Services_DataGrid.SelectAll()   #Because we use checkboxes for row selection we don't need to set all rows as selected
 })
- #>
+
+$Services_HeaderChkBox.add_UnChecked({ 
+    $Services_Datatable | ForEach-Object {$_.CheckboxSelect = $false}
+    #$Services_DataGrid.SelectedItems.Clear()   #Because we use checkboxes for row selection we don't need to set all rows as not-selected
+})
+
+$Services_DataGrid.add_SelectionChanged({ #Because we use checkboxes for row selection we have to disable the row click selection
+    $_.Handled = $true
+})
+
 $Services_DataGrid.Add_GotMouseCapture({
-    $DisplayIndex = $this.CurrentColumn.DisplayIndex
+    $DisplayIndex = $this.CurrentColumn.DisplayIndex   # Returns the Column index (0 based) where the mouse was clicked on.
+    $CurrentItemName = $this.CurrentItem.Name
     $OriginalSourceName = $_.OriginalSource.Name
+    $OriginalSource = $_.OriginalSource
     switch (($_.OriginalSource).GetType().name) {
         "CheckBox" { 
-                    if (!$OriginalSourceName) {   # Exclude header checkbox
-                        write-host " Checkbox checked on column $DisplayIndex"
-                        $this.CurrentItem | out-host }
+                        if (!$OriginalSourceName) {   # Ignore header checkbox click (OriginalSource.Name in row checkboxes is null )
+                            $Row = $Services_Datatable.select("Name = '$($CurrentItemName)'")
+                            $RowIndex = $Services_Datatable.Rows.IndexOf($Row[0])
+                            $Services_Datatable.Rows[$RowIndex].CheckboxSelect = !($Services_Datatable.Rows[$RowIndex].CheckboxSelect)
+                            $OriginalSource.IsChecked = !($OriginalSource.IsChecked) # It has to reflip to get to the right state.
+                        }
+                        break
                     }
         "Button"    {
-                        write-host "button $OriginalSourceName checked"
-                        $this.CurrentCell.item | out-host
+                        switch ($OriginalSourceName) {
+                            "Services_DeleteButton" {$CurrentItemName | out-host}
+                            "Services_InsertButton" {write-host "koko"}
+                        }
+                        break
                     }
     }
+    $Services_Datatable | Where-Object {$_.CheckboxSelect -eq $true} | out-host
 })
 
-$Services_DataGrid.Add_PreviewKeyDown({
+$Services_DataGrid.Add_PreviewKeyDown({   #Prevent using keyboard on this DataGrid
     $_.Handled = $true
 })
 
